5783cb99887c3b1bed515e3cd9b4bf69e2a86929
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-05-29 02:21:36 -0700

    Update status-deceiver command to print CDJ Statuses

--- cmd/status-reciever/main.go
+++ cmd/status-reciever/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"go.evanpurkhiser.com/prolink"
-	"go.evanpurkhiser.com/prolink/trackstatus"
 )
 
 func main() {
@@ -35,17 +34,9 @@ func main() {
 	dm.OnDeviceAdded(prolink.DeviceListenerFunc(added))
 	dm.OnDeviceRemoved(prolink.DeviceListenerFunc(removed))
 
-	trackChangeConfig := trackstatus.Config{
-		AllowedInterruptBeats: 8,
-		BeatsUntilReported:    128,
-	}
-
-	changed := func(devID prolink.DeviceID, track uint32, status trackstatus.Status) {
-		fmt.Printf("Track ID %d on device %d is now in %s\n", track, devID, status)
-	}
-
-	trackStatusHandler := trackstatus.NewHandler(trackChangeConfig, changed)
-	dj.OnStatusUpdate(trackStatusHandler)
+	dj.OnStatusUpdate(prolink.StatusHandlerFunc(func(s *prolink.CDJStatus) {
+		fmt.Println(s)
+	}))
 
 	<-make(chan bool)
 }

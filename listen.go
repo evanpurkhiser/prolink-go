bc799d2fc4ff433cb38039b2f6fb60994de3207a
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-10-09 00:45:54 -0700

    Remove experimental pcap listener

--- listen.go
+++ listen.go
@@ -4,72 +4,12 @@ import (
 	"fmt"
 	"io"
 	"net"
-
-	"github.com/google/gopacket"
-	"github.com/google/gopacket/pcap"
 )
-
-// captureListener implements io.Reader, providing the ability to capture
-// status packets using pcap, instead of binding to the interface itself.
-//
-// This allows the software to run along side other programs that listen for
-// status packets (such as rekordbox).
-type captureListener struct {
-	source *gopacket.PacketSource
-}
-
-// Read implements the io.Reader interface. This method will read a status
-// packet directly off the interface using packet capturing.
-func (cl *captureListener) Read(p []byte) (int, error) {
-	for packet := range cl.source.Packets() {
-		appLayer := packet.ApplicationLayer()
-		if appLayer == nil {
-			continue
-		}
-
-		data := appLayer.Payload()
-		copy(p, data)
-
-		return len(data), nil
-	}
-
-	return 0, nil
-}
-
-// newCaptureListener attempts to create a captureListener. In the case where
-// we cannot sniff the network interface this may fail due to privileges.
-func newCaptureListener(iface *net.Interface, addr *net.UDPAddr) (*captureListener, error) {
-	handle, err := pcap.OpenLive(iface.Name, 1600, false, pcap.BlockForever)
-	if err != nil {
-		return nil, err
-	}
-
-	// Compile a BPF filter to listen for UDP packets on the given port
-	handle.SetBPFFilter(fmt.Sprintf("udp port %d", addr.Port))
-
-	// We're listening for incoming packets only
-	handle.SetDirection(pcap.DirectionIn)
-
-	src := gopacket.NewPacketSource(handle, handle.LinkType())
-
-	listener := captureListener{
-		source: src,
-	}
-
-	return &listener, nil
-}
 
 // openListener crates a status listener connection (returned as an io.Reader).
 // If sniff is enabled we will attempt to listen on the interface using packet
 // capturing, otherwise, we will simply directly bind to the interface.
 func openListener(iface *net.Interface, addr *net.UDPAddr, sniff bool) (io.Reader, error) {
-	if sniff {
-		captureListener, err := newCaptureListener(iface, addr)
-		if err == nil {
-			return captureListener, nil
-		}
-	}
-
 	listenerConn, err := net.ListenUDP("udp", listenerAddr)
 	if err == nil {
 		return listenerConn, nil
